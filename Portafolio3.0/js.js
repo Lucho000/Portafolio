95c9288c6e3244e28b60c9b8c663444579f0a3b8
Lucho000 <lucianodavidsposito@gmail.com>
2023-02-08 04:02:19 -0300

    ya falta poquito

--- Portafolio3.0/js.js
+++ Portafolio3.0/js.js
@@ -1,5 +1,11 @@
 /*CHANGE MODE*/
 var toggle = document.getElementById('toggle');
+var body = document.getElementById('body');
+var uno = document.getElementById('navPrincipal');
+var dos = document.getElementById('toggle');
+var tres = document.getElementById('toggle');
+var cuatro = document.getElementById('toggle');
 toggle.onclick=()=>{
     toggle.classList.toggle('active');
+    body.style.color='var(--color5)';
 }
